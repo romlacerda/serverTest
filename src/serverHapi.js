fa10463f576680e5e880ed7a2b3b6dd6e18113a7
Rômulo de Lacerda Correa Rimulo <romulo.rimulo@radixeng.com.br>
2018-06-01 13:03:33 -0300

    With Hapi.js

---
+++ src/serverHapi.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const Hapi = require('hapi')
+
+const server = Hapi.server({
+    host: 'localhost',
+    port: 80
+})
+
+server.route({
+    method: 'GET',
+    path:'/hello',
+    handler: function(request, h) {
+        return 'Hello World'
+    }
+})
+
+async function start() {
+    try {
+        await server.start()
+    }
+    catch (err) {
+        console.log(err)
+        process.exit(1)
+    }
+    console.log('Server running at: ', server.info.uri)
+
+}
+
+start()
